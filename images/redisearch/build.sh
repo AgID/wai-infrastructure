28d97896cc0a221dd94da86a9e6bcd75b172e8af
Davide Porrovecchio <pdavide@gmail.com>
2020-02-24 18:17:37 +0100

    Update image tag

--- images/redisearch/build.sh
+++ images/redisearch/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker build -t webanalyticsitalia/wai-redisearch:5.0.7-dev-2 --build-arg REDIS_VERSION=5.0.7 --build-arg REDISEARCH_GITHUB_BRANCH=v1.6.8 .
+docker build -t webanalyticsitalia/wai-redisearch:5.0.7-stable --build-arg REDIS_VERSION=5.0.7 --build-arg REDISEARCH_GITHUB_BRANCH=v1.6.8 .
