4b6d80f96868a67957df3f146483d3249b90edd1
Gianluca Fares <gianluca.fares@giafar.it>
2021-09-12 12:41:02 +0200

    WTF 3

--- images/wai-openresty/tests/test.sh
+++ images/wai-openresty/tests/test.sh
@@ -3,5 +3,5 @@ docker-compose up -d && \
 sleep 5 && \
 docker-compose exec redis sh -c "echo SET 1 \\\"https://www.site-one.it https://site-two.it https://site-three.it\\\" | redis-cli" && \
 docker-compose exec redis sh -c "echo \"CONFIG SET protected-mode no\" | redis-cli" && \
-newman run WAI-Openresty.postman_collection.json && \
+/home/travis/.nvm/versions/node/v8.12.0/bin/newman run WAI-Openresty.postman_collection.json && \
 docker-compose rm -f -s -v
