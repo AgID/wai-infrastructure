157c9bd15bb49bcecc2097e03e2a2b3cd3b2f900
Davide Porrovecchio <pdavide@gmail.com>
2021-12-14 10:39:31 +0100

    Bump wai-openresty image version

--- images/wai-openresty/build.sh
+++ images/wai-openresty/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker build -t webanalyticsitalia/wai-openresty:1.0.2-stable -t webanalyticsitalia/wai-openresty:latest --build-arg OPENRESTY_VERSION=1.19.3.1-2-alpine-fat .
-docker push webanalyticsitalia/wai-openresty:1.0.2-stable
+docker build -t webanalyticsitalia/wai-openresty:1.1.0-stable -t webanalyticsitalia/wai-openresty:latest --build-arg OPENRESTY_VERSION=1.19.3.1-2-alpine-fat .
+docker push webanalyticsitalia/wai-openresty:1.1.0-stable
 docker push webanalyticsitalia/wai-openresty:latest
