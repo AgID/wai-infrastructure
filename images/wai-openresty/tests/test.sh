ec7d9f922001209213188caa1c8934d96791cec0
Davide Porrovecchio <pdavide@gmail.com>
2021-12-14 11:47:42 +0100

    Use GitHub actions CI

--- images/wai-openresty/tests/test.sh
+++ images/wai-openresty/tests/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-docker-compose up -d && \
+docker compose up -d && \
 sleep 5 && \
-docker-compose exec redis sh -c "echo SET 1 \\\"www.site-one.it site-two.it site-three.it\\\" | redis-cli" && \
-docker-compose exec redis sh -c "echo \"CONFIG SET protected-mode no\" | redis-cli" && \
+docker compose exec redis sh -c "echo SET 1 \\\"www.site-one.it site-two.it site-three.it\\\" | redis-cli" && \
+docker compose exec redis sh -c "echo \"CONFIG SET protected-mode no\" | redis-cli" && \
 newman run WAI-Openresty.postman_collection.json && \
-docker-compose rm -f -s -v
+docker compose rm -f -s -v
