936855ebd1b685ccf9d7e032c8428db7f0f18a47
Davide Porrovecchio <pdavide@gmail.com>
2023-02-15 18:32:23 +0100

    Update redis version

--- images/redisearch/build.sh
+++ images/redisearch/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker build -t webanalyticsitalia/wai-redisearch:5.0.12-stable -t webanalyticsitalia/wai-redisearch:latest --build-arg REDIS_VERSION=5.0.12 --build-arg REDISEARCH_VERSION=v1.6.15 .
+docker build -t webanalyticsitalia/wai-redisearch:5.0.14-stable -t webanalyticsitalia/wai-redisearch:latest --build-arg REDIS_VERSION=5.0.14 --build-arg REDISEARCH_VERSION=v1.6.15 .
